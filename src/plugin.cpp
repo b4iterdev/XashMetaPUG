e97f792a6468acb23f59c0ecaec95e6c3dcc1ab9
b4iterdev <thaimeo200389@gmail.com>
2026-06-08 19:25:12 +0700

    Revert "KillRandomPlayer: use CBasePlayer::TakeDamage instead of direct entvars"
    This reverts commit 31ff41176128706ba238644940f53d745f3b5ad4.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -706,24 +706,20 @@ void Plugin::AssignRandomModelForTeam(edict_t *entity, Team team)
 
 void Plugin::KillRandomPlayer()
 {
-    std::vector<edict_t *> alive;
+    std::vector<edict_t *> candidates;
     for (int i = 1; i <= kMaxClients; ++i) {
         if (!players_[i].connected) continue;
         edict_t *entity = INDEXENT(i);
         if (FNullEnt(entity)) continue;
-        if (entity->v.health > 0 && entity->v.deadflag == DEAD_NO) {
-            alive.push_back(entity);
-        }
+        candidates.push_back(entity);
     }
-    if (alive.empty()) {
+    if (candidates.empty()) {
         return;
     }
-    edict_t *victim = alive[std::rand() % alive.size()];
-    CBasePlayer *player = CBasePlayer::Instance(victim);
-    if (player) {
-        player->TakeDamage(&victim->v, &victim->v, 1000.0f, DMG_GENERIC);
-        Log("[XMP] Triggered TakeDamage kill on player #%d to pre-fire #Game_Commencing.\n", PlayerIndex(victim));
-    }
+    edict_t *victim = candidates[std::rand() % candidates.size()];
+    victim->v.health = 0;
+    victim->v.deadflag = DEAD_DEAD;
+    Log("[XMP] Triggered entvars kill on player #%d to pre-fire #Game_Commencing.\n", PlayerIndex(victim));
 }
 
 int Plugin::RandomClassSlotForTeam(Team team) const
